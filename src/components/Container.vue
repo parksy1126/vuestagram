837bdfbc6e28a10bce68be6d0ed71bb3442481db
park1126 <keilman1126@gmail.com>
2022-05-29 11:10:41 +0900

    vuestargram

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    
+  },
+  
+}
+</script>
+
+
+<style>
+
+</style>
